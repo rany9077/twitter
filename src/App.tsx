7c60a9e6866737953680aa9a69103be2147c3f76
ran <rranyyy9077@gmail.com>
2024-06-04 17:28:56 +0900

    회원가입/로그인 기능 생성(구글,깃헙 기능 추가)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
+import { useState, useEffect } from "react";
+
 import Router from 'pages/components/Router';
 import { Layout } from "pages/components/Layout"
 
+import { getAuth, onAuthStateChanged } from "firebase/auth"
+import { app } from 'firebaseApp';
+import { ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+import Loader from "pages/components/loader/Loader";
+
 function App() {
+  const auth = getAuth(app);
+  const [init, setInit] = useState<boolean>(false);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!auth?.currentUser
+  );
+
+  // console.log(isAuthenticated)
+
+  useEffect(()=>{
+    onAuthStateChanged(auth, (user) => {
+      if(user) {
+        setIsAuthenticated(true);
+      } else{
+        setIsAuthenticated(false);
+      }
+      setInit(true);
+    })
+  },[auth])
+
   return (
     <Layout>
-      <Router />
+      <ToastContainer theme="dark" autoClose={1000} hideProgressBar newestOnTop/>
+      {init ? <Router isAuthenticated={isAuthenticated}/> : <Loader />}
     </Layout>
   );
 }
--- src/pages/components/Menu.tsx
+++ src/pages/components/Menu.tsx
@@ -1,9 +1,16 @@
 import { BsHouse } from "react-icons/bs";
 import { FaRegUserCircle } from "react-icons/fa";
-import { MdOutlineLogout } from "react-icons/md";
-import { useNavigate } from "react-router-dom"
+import { MdOutlineLogout, MdLogin } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import AuthContext from "context/AuthContext";
+import { getAuth, signOut } from "firebase/auth";
+import { toast } from "react-toastify";
+import { app } from "firebaseApp";
 
 export default function MenuList(){
+    const {user} = useContext(AuthContext);
+    // console.log(user);
     const navigate = useNavigate();
     return ( 
     <div className="footer">
@@ -16,10 +23,21 @@ export default function MenuList(){
                 <FaRegUserCircle />
                 Profile
             </button>
-            <button type="button" onClick={() => navigate("/")}>
-                <MdOutlineLogout />
-                Logout
-            </button>
+            {user === null ? (
+                <button type="button" onClick={() => navigate("/users/login")}>
+                    <MdLogin />
+                    Login
+                </button>
+            ) : (
+                <button type="button" onClick={ async () => {
+                    const auth = getAuth(app);
+                    await signOut(auth);
+                    toast.success('로그아웃 되었습니다.')
+                }}>
+                    <MdOutlineLogout />
+                    Logout
+                </button>
+            )}
         </div>
     </div>
     )

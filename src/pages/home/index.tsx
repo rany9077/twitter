d2a0824c57d4bd3f1651f4f839bbe5e8423c3d86
ran <rranyyy9077@gmail.com>
2024-06-04 17:57:53 +0900

    firestore로 게시글 리스트  가져오기

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -54,10 +54,12 @@ const posts: PostProps[] = [
 export default function HomePage(){
     return (
         <div className="home">
-            <div className="home__title">Home</div>
-            <div className="home__tabs">
-                <div className="home__tab home__tab--active">For You</div>
-                <div className="home__tab">Following</div>
+            <div className="home__top">
+                <div className="home__title">Home</div>
+                <div className="home__tabs">
+                    <div className="home__tab home__tab--active">For You</div>
+                    <div className="home__tab">Following</div>
+                </div>
             </div>
 
             <PostForm />
